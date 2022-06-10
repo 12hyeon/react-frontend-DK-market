188924544b2ce9176bd720cf887cfb4fb946f1d0
이현정 <1212guswjd@gmail.com>
2022-06-11 01:44:40 +0900

    modified thing-page

--- src/components/create.js
+++ src/components/create.js
@@ -4,70 +4,32 @@ import React from "react";
 
 function Create(props) {
   const [title, setTitle] = useState("")
-
+  const [price, setPrice] = useState("0")
+  const [text, setText] = useState("")
+  const [file, setFile] = useState("")
+    
   return (
     <div className="create">
         <form>
-        <h2>물건 등록 페이지</h2>
-        <div>
-            <input id="title" placeholder="제목을 입력해주세요" value={title} className="create__input"/>
-            <input id="price" placeholder="가격을 입력해주세요" className="create__input"/>
+          <h2>물건 등록 페이지</h2>
+          <div>
+            <input id="title" placeholder="제목을 입력해주세요" className="create__input" onChange={(event) => setTitle(event.target.value)}/>
+            <input id="price" placeholder="가격을 입력해주세요" className="create__input"  onChange={(event) => setPrice(event.target.value)}/>
             <textarea
-              id="text"
+              id="text"  onChange={(event) => setText(event.target.value)}
               placeholder="물건 설명을 입력해주세요." className="create__text"/>
-          <input
-            type="file"
-            id="real-input"
-            class="image_inputType_file"
-            accept="img/*"
-            required
-            multiple/>
-          <input type="button" value="사진업로드" />
+            <input
+              type="file"
+              id="real-input"
+              class="image_inputType_file"
+              onChange={(event) => setFile(event.target.value)}
+              accept="img/*"
+              multiple/>
           </div>
-          <div><button type="submit" className="create__button">제품 올리기</button>
+          <div><button type="submit" className="create__button"  onClick={()=>alert("title : "+title+"\nprice : "+price+"\ntext : "+text+"\nfile path : "+file)}>제품 올리기</button>
           </div>
         </form>
     </div>
   );
 }
 export default Create;
-
-/*
-
-    <div class="box">
-      <div class="push">
-        <ul>
-          <li>
-            <h1>물건 등록 페이지</h1>
-          </li>
-          <li>
-            <input id="title" placeholder="제목을 입력해주세요"></input>
-          </li>
-          <li>
-            <input id="price" placeholder="가격을 입력해주세요"></input>
-          </li>
-          <li>
-            <textarea
-              id="text"
-              placeholder="물건 설명을 입력해주세요."
-            ></textarea>
-          </li>
-          <hr />
-          <li>
-            <input
-              type="file"
-              id="real-input"
-              class="image_inputType_file"
-              accept="img/*"
-              required
-              multiple
-            ></input>
-            <button class="browse-btn">사진업로드</button>
-          </li>
-          <li>
-            <button value="제품 올리기"></button>
-          </li>
-        </ul>
-      </div>
-    </div>
-    */
--- src/components/item.js
+++ src/components/item.js
@@ -1,5 +1,6 @@
-import "../styles.css";
+import "./item.css";
 import React from "react";
+import {useState} from 'react'
 
 // text가 일정한 길이면 아래줄이 되게 해야함
 const item = [
@@ -7,34 +8,36 @@ const item = [
     id: 1,
     title: "교재1111111",
     price: "1234",
-    text: "~~~~~~~~~~~~~~~~~~~~~~",
+    text: "~~~~~~~~~~~~~~~~~~~~~~\n~~~~~~~~~~~~~~~~~~~~~~\n~~~~~~~~~~~~~~~~~~~~~~",
     img: "/교재.jpg",
     heart: "1",
     watch: "2"
   }
 ];
 function Item(props) {
+  const [name, setName] = useState("nickname")
+  const [comment, setComment] = useState("")
   return (
     <div>
       <div class="watch">
         <ul>
           <li>
-            <h1>상세 물건 페이지</h1>
+            <h2>상세 제품 페이지</h2>
           </li>
           <li>
             <div class="boxes">
               <img class="box-l" src="/" alt="제품 이미지"></img>
               <div class="box-r">
-                <h2>{item[0].title}</h2>
-                <h2 id="item-price">{item[0].price}원</h2>
+                <h3 id="title"><b>{item[0].title}</b></h3>
+                <h3 id="item-price"><b>{item[0].price}원</b></h3>
                 <div class="card-b-r">
-                  <h4>
+                  <h5>
                     watch {item[0].watch} | heart {item[0].heart}
-                  </h4>
+                  </h5>
                 </div>
-                <input type="button" value="관심"></input>
-                <input type="button" value="연락하기"></input>
-                <input type="button" value="구매신청"></input>
+                <input id="watch" type="button" value="관심"></input>
+                <input id="call" type="button" value="연락하기"></input>
+                <input id="click" type="button" value="구매신청"></input>
               </div>
             </div>
           </li>
@@ -43,17 +46,18 @@ function Item(props) {
             <div id="item-text">{item[0].text}</div>
           </li>
           <li>
-            <div id="comment">
-              <h3 id="count">댓글 0 </h3>
+            <form id="comment">
+              <h4 id="count">댓글 0 </h4>
               <input
-                id="comment-input"
+                id="comment-input" name="answer"
                 placeholder="댓글을 입력해 주세요."
+                onChange={(event) => setComment(event.target.value)}
               ></input>
-              <button id="submit">등록</button>
-            </div>
+              <button id="submit" onClick={()=>alert("name : "+name+"\ncomment : "+comment)}>등록</button>
+            </form>
           </li>
           <li>
-            <input type="button" value="목록"></input>
+            <input id="list" type="button" value="목록"></input>
           </li>
         </ul>
       </div>
